a72ebb4d36f0f0cb926e80de0358ccfaf2bdf51e
USER PC <surapha.nu@rmuti.ac.th>
2020-08-25 16:26:38 +0700

    addclassTitleFrakment

--- app/src/main/java/android/example/com/firstapp/TitleFragment.kt
+++ app/src/main/java/android/example/com/firstapp/TitleFragment.kt
@@ -1,4 +1,17 @@
 package android.example.com.firstapp
 
-class TitleFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
+            R.layout.fragment_title,container,false)
+        return binding.root
+
+    }
 }
